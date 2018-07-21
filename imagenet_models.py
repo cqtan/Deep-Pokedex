f9f164b4f2937d98cbd1fb29ccf052c48954bf0d
CouchCat <s0547139@htw-berlin.de>
2018-07-21 15:23:23 +0200

    Init

---
+++ imagenet_models.py
@@ -0,0 +1,27 @@
+from keras.applications import mobilenet, resnet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+import coremltools
+
+
+mobilenet_model = mobilenet.MobileNet(weights='imagenet')
+resnet_model = resnet50.ResNet50(weights='imagenet')
+
+imagenet_labels_dict = 
+
+coreml_mobilenet_model = coremltools.converters.keras.convert(mobilenet_model,
+	input_names="image",
+	image_input_names="image",
+	is_bgr=True)
+
+print("[INFO] saving model as mobilenet.mlmodel")
+coreml_mobilenet_model.save("mobilenet.mlmodel")
+
+coreml_resnet_model = coremltools.converters.keras.convert(resnet_model,
+	input_names="image",
+	image_input_names="image",
+	is_bgr=True)
+
+print("[INFO] saving model as mobilenet.mlmodel")
+coreml_mobilenet_model.save("resnet_model.mlmodel")
